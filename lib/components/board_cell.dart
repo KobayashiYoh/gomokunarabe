bc7ef1a820fbe5b66bd8dfbfdc7c8ae8886bb9ef
KobayashiYoh <y.kobayashi@4nonome.com>
2023-06-24 18:25:41 +0900

    BoardCellの背景の線を追加

--- lib/components/board_cell.dart
+++ lib/components/board_cell.dart
@@ -1,29 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:gomokunarabe/models/cell_type.dart';
 
 class BoardCell extends StatelessWidget {
   const BoardCell({Key? key, required this.index}) : super(key: key);
   final int index;
 
+  final minIndex = 0;
+  final maxIndex = 224;
+
+  String get argumentErrorMessage =>
+      'Invalid index value. Please provide a value between $minIndex and $maxIndex.';
+
+  CellType get cellType {
+    if (index < minIndex || index > maxIndex) {
+      throw ArgumentError(argumentErrorMessage);
+    } else if (index == minIndex) {
+      return CellType.topLeft;
+    } else if (index == 14) {
+      return CellType.topRight;
+    } else if (index == 210) {
+      return CellType.bottomLeft;
+    } else if (index == maxIndex) {
+      return CellType.bottomRight;
+    } else if (index >= 1 && index <= 13) {
+      return CellType.topSide;
+    } else if (index >= 211 && index <= 223) {
+      return CellType.bottomSide;
+    } else if (index % 15 == 0) {
+      return CellType.leftSide;
+    } else if ((index - 14) % 15 == 0) {
+      return CellType.rightSide;
+    } else {
+      return CellType.inside;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        print(index);
-      },
+      onTap: () {},
       child: Container(
-        margin: const EdgeInsets.all(1.0),
         height: double.infinity,
         alignment: Alignment.center,
         decoration: BoxDecoration(
-          color: const Color(0xfffdfcd1),
+          color: const Color(0xFFFDE2BB),
           borderRadius: BorderRadius.circular(1.0),
+          image: DecorationImage(
+            image: AssetImage(cellType.imagePath),
+          ),
         ),
         child: FittedBox(
-          child: Text(
-            index.toString(),
-            style: const TextStyle(
-              fontSize: 12.0,
-            ),
+          child: Stack(
+            children: [
+              Container(),
+              Text(
+                index.toString(),
+                style: const TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.green,
+                ),
+              ),
+            ],
           ),
         ),
       ),
---
+++ lib/constants/image_paths.dart
@@ -0,0 +1,11 @@
+class ImagePaths {
+  static const String cellTopLeft = 'assets/images/CellTopLeft.png';
+  static const String cellTopSide = 'assets/images/CellTopSide.png';
+  static const String cellTopRight = 'assets/images/CellTopRight.png';
+  static const String cellLeftSide = 'assets/images/CellLeftSide.png';
+  static const String cellInside = 'assets/images/CellInside.png';
+  static const String cellRightSide = 'assets/images/CellRightSide.png';
+  static const String cellBottomLeft = 'assets/images/CellBottomLeft.png';
+  static const String cellBottomSide = 'assets/images/CellBottomSide.png';
+  static const String cellBottomRight = 'assets/images/CellBottomRight.png';
+}
