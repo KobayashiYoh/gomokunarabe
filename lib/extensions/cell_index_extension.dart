5b6f101be4a06d5a11e089c00466710bf6f4ada5
KobayashiYoh <y.kobayashi@4nonome.com>
2023-07-03 23:08:37 +0900

    CellIndexExtensionを修正

--- lib/extensions/cell_index_extension.dart
+++ lib/extensions/cell_index_extension.dart
@@ -3,21 +3,26 @@ import 'package:gomokunarabe/constants/cell_index.dart';
 extension CellIndexExtension on int {
   bool get isErrorCellIndex =>
       this < CellIndex.minIndex || this > CellIndex.maxIndex;
+  bool get isNotErrorCellIndex => !isErrorCellIndex;
+
   bool get isWallIndex =>
       (this >= CellIndex.minIndex && this <= 16) ||
       (this >= 272 && this <= CellIndex.maxIndex) ||
-      (this % 17 == 0) ||
-      ((this + 1) % 17 == 0);
-  bool get isTopLeftCellIndex => this == CellIndex.topLeftIndex;
-  bool get isTopRightCellIndex => this == CellIndex.topRightIndex;
-  bool get isBottomLeftCellIndex => this == CellIndex.bottomLeftIndex;
-  bool get isBottomRightCellIndex => this == CellIndex.bottomRightIndex;
-  bool get isTopSideCellIndex =>
-      this > CellIndex.topLeftIndex && this < CellIndex.topRightIndex;
-  bool get isBottomSideCellIndex =>
-      this > CellIndex.bottomLeftIndex && this < CellIndex.bottomRightIndex;
-  bool get isLeftSideCellIndex => (this - 1) % 17 == 0;
-  bool get isRightSideCellIndex => (this + 2) % 17 == 0;
+      (isNotErrorCellIndex && this % 17 == 0) ||
+      (isNotErrorCellIndex && (this + 1) % 17 == 0);
+  bool get isNotWallIndex => !isWallIndex;
+
+  bool get isTopLeftCellIndex => this == 18;
+  bool get isTopRightCellIndex => this == 32;
+  bool get isBottomLeftCellIndex => this == 256;
+  bool get isBottomRightCellIndex => this == 270;
+
+  bool get isTopSideCellIndex => this >= 18 && this <= 32;
+  bool get isBottomSideCellIndex => this >= 256 && this <= 270;
+
+  bool get isLeftSideCellIndex => isNotWallIndex && (this - 1) % 17 == 0;
+  bool get isRightSideCellIndex => isNotWallIndex && (this + 2) % 17 == 0;
+
   bool get isInsideWithDotCellIndex =>
       this == 72 || this == 80 || this == 208 || this == 216;
 }
